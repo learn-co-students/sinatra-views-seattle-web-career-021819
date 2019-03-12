3de80f6a84a413bcfa38d7e7d0886ca9d4d260fb
ehatlebe <github email address>
2019-03-11 19:04:37 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+	# can create as many views as we want
 
 	get '/' do
-		
+		# tells Sinatra to render a file called index.erb inside of a directory called views
+		erb :index
+	end
+
+	get "/info" do
+		erb :info
 	end
 end
